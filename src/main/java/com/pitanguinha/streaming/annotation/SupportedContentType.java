e0252eb90bff9c1ba031571ce1b009811f0d8a28
luigiPereira <luigipereira1001@gmail.com>
2025-07-01 12:10:39 -0300

    feat(media): annotation for help validate the content type
    Annotation: validate the correct content type for field
    Util: help to reuse the validation for any supported types
    Audio: supported types for audio files
    Thumbnail: supported types for image files

---
+++ src/main/java/com/pitanguinha/streaming/annotation/SupportedContentType.java
@@ -0,0 +1,67 @@
+package com.pitanguinha.streaming.annotation;
+
+import java.lang.annotation.*;
+
+import org.springframework.http.codec.multipart.FilePart;
+
+import com.pitanguinha.streaming.enums.media.contenttypes.SupportedTypeUtil;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import jakarta.validation.*;
+
+/**
+ * Custom annotation for validating that a file part's content type is one of
+ * the supported audio formats.<br>
+ * This annotation can be applied to fields or parameters.<br>
+ * The caller should handler the null or empty file part case, as this
+ * annotation only checks the content type of the file part.<br>
+ * Supported content types include:
+ * <ul>
+ * Audio Formats: opus, ogg, flac, mp3, wav, wav_x, aac.
+ * </ul>
+ * <ul>
+ * Image Formats: png, jpeg, webp.
+ * </ul>
+ * 
+ * @since 1.0
+ */
+@Documented
+@Retention(RUNTIME)
+@Target({ FIELD, PARAMETER })
+@Constraint(validatedBy = SupportedContentTypeValidator.class)
+public @interface SupportedContentType {
+    String message() default "Invalid content type.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    Class<? extends Enum<?>> enumClass();
+}
+
+class SupportedContentTypeValidator implements ConstraintValidator<SupportedContentType, FilePart> {
+    private Class<? extends Enum<?>> enumClass;
+
+    @Override
+    public void initialize(SupportedContentType constraintAnnotation) {
+        this.enumClass = constraintAnnotation.enumClass();
+    }
+
+    @Override
+    public boolean isValid(FilePart filePart, ConstraintValidatorContext context) {
+        if (filePart == null || filePart.filename() == null)
+            return true; // Null or empty file part is valid
+
+        String contentType = filePart.headers().getContentType() != null
+                ? filePart.headers().getContentType().toString()
+                : null;
+
+        if (contentType == null || contentType.isEmpty()) {
+            return false; // Invalid content type
+        }
+
+        return SupportedTypeUtil.isSupported(enumClass, contentType);
+    }
+}
